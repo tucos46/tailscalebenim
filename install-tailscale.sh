d761512dc1b18d0fe35db93c3514831bf3b19a4b
tucos46 <111047422+tucos46@users.noreply.github.com>
2025-06-23 09:56:44 +0300

    Delete install-tailscale.sh

--- install-tailscale.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
-wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
-cp -r tailscale tailscaled /render/
-
-mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
